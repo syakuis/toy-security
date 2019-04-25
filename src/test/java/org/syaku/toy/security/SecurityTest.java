63b36d2c4776aa5e39ab84d43d7ce0aa88204ce6
syaku <syaku@naver.com>
2019-04-25 15:41:57 +0900

    필요없는 코드 제거

--- src/test/java/org/syaku/toy/security/SecurityTest.java
+++ src/test/java/org/syaku/toy/security/SecurityTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,8 +26,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SecurityTest {
 
     @Autowired private MockMvc mvc;
-
-    @Autowired private PasswordEncoder passwordEncoder;
 
     @Test
     public void 로그인() throws Exception {
