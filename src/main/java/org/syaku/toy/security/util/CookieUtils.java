64314ae57cf71bffa7b30525cff37bd4d9545de4
syaku <syaku@naver.com>
2019-04-29 18:50:07 +0900

    연동 작업중.

---
+++ src/main/java/org/syaku/toy/security/util/CookieUtils.java
@@ -0,0 +1,37 @@
+package org.syaku.toy.security.util;
+
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * https://hellokoding.com/hello-single-sign-on-sso-with-json-web-token-jwt-spring-boot/
+ * @author Seok Kyun. Choi.
+ * @since 2019-04-29
+ */
+public class CookieUtils {
+    public static void create(HttpServletResponse response, String name, String value, Boolean secure, Integer maxAge, String domain) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setSecure(secure);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        cookie.setDomain(domain);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    public static void clear(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    public static String getValue(HttpServletRequest request, String name) {
+        Cookie cookie = WebUtils.getCookie(request, name);
+        return cookie != null ? cookie.getValue() : null;
+    }
+}
